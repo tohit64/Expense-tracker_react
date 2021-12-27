3bf60091757b6dcb020059bf9eac2414f2856cf2
tohit64 <tohitkhan.cr7@gmail.com>
2021-12-27 15:33:08 +0530

    Added Expenses Items

---
+++ src/components/Expenses.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { ExpenseItem } from "./ExpenseItem";
+import Card from "./Card";
+import "./Expenses.css";
+
+function Expenses(props) {
+  const expenses = props.expenses;
+  return (
+    <Card className="expenses">
+      <ExpenseItem
+        date={expenses[0].date}
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+      />
+      <ExpenseItem
+        date={expenses[1].date}
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+      />
+      <ExpenseItem
+        date={expenses[2].date}
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+      />
+      <ExpenseItem
+        date={expenses[3].date}
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+      />
+    </Card>
+  );
+}
+
+export default Expenses;
