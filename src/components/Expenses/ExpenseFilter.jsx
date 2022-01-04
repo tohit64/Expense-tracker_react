5a048eff231569ad3ae1de683d50871951801707
Tohit Khan <tohitkhan.cr7@gmail.com>
2022-01-04 11:56:42 +0530

    initial

--- src/components/Expenses/ExpenseFilter.jsx
+++ src/components/Expenses/ExpenseFilter.jsx
@@ -7,7 +7,7 @@ function ExpenseFilter() {
       <div className="expenses-filter__control">
         <label>Filter By Year</label>
         <select>
-          <option value="2019">2019</option>
+          <option value="2019">2010</option>
           <option value="2020">2020</option>
           <option value="2021">2021</option>
           <option value="2022">2022</option>
