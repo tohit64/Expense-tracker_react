c13c0a07f2d7c35af323142b5bb634b6e36155f4
Tohit Khan <tohitkhan.cr7@gmail.com>
2022-01-05 16:57:43 +0530

    added event on add expense

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -81,7 +81,8 @@ function ExpenseForm(props) {
         </div>
       </div>
       <div className="new-expense__actions">
-        <button>Add Expense</button>
+        <button type="button" onClick={props.onCancel}>Cancel</button>
+        <button type="sumbit">Add Expense</button>
       </div>
     </form>
   );
--- src/components/NewExpense/NewExpense.jsx
+++ src/components/NewExpense/NewExpense.jsx
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 function NewExpense(props) {
+  const [isEditing, setIsEditing] = useState(false);
+
   function sumbitDataHandler(expenseData) {
     const newExpenseData = {
       ...expenseData,
       id: Math.random().toString(),
     };
     props.onSaveDataHandler(newExpenseData);
+    setIsEditing(false);
+  }
+
+  function startEditingHandler() {
+    setIsEditing(true);
+  }
+
+  function stopEditingHandler() {
+    setIsEditing(false);
   }
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSumbitData={sumbitDataHandler} />
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add Expenses</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onSumbitData={sumbitDataHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
     </div>
   );
 }
