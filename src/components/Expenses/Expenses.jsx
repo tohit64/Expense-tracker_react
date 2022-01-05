652cf29c79d17fd2fe9b90e09759091c62ec1894
Tohit Khan <tohitkhan.cr7@gmail.com>
2022-01-05 15:22:24 +0530

    added key

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -22,7 +22,12 @@ export function Expenses(props) {
     <Card className="expenses">
       <ExpenseFilter selected={filterYear} onYearChange={yearChangeHandler} />
       {filteredExpepenses.map((item) => (
-        <ExpenseItem title={item.title} amount={item.amount} date={item.date} />
+        <ExpenseItem
+          key={item.id}
+          title={item.title}
+          amount={item.amount}
+          date={item.date}
+        />
       ))}
     </Card>
   );
